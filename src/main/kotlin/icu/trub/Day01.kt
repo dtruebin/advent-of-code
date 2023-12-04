ddc9cb238ac753eb6f972a41a90253440c54498d
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2023-12-04 12:12:12 +0100

    Day 1: refactor - extract method

--- src/main/kotlin/icu/trub/Day01.kt
+++ src/main/kotlin/icu/trub/Day01.kt
@@ -1,19 +1,24 @@
 package icu.trub
 
+import java.util.stream.Stream
+
 fun main() {
     println("Day 1, part 1: ${solveDay1a("day01.txt")}")
     println("Day 1, part 2: ${solveDay1b("day01.txt")}")
 }
 
 fun solveDay1a(inputFileName: String): Int =
-    object {}.javaClass.getResourceAsStream(inputFileName)!!.bufferedReader().lines()
+    readTxtResource(inputFileName)
         .map(::getDoubleDigitFromFirstAndLast)
         .toList()
         .sum()
 
 fun solveDay1b(inputFileName: String): Int =
-    object {}.javaClass.getResourceAsStream(inputFileName)!!.bufferedReader().lines()
+    readTxtResource(inputFileName)
         .map(::replaceWordsWithDigits)
         .map(::getDoubleDigitFromFirstAndLast)
         .toList()
         .sum()
+
+private fun readTxtResource(inputFileName: String): Stream<String> =
+    object {}.javaClass.getResourceAsStream(inputFileName)!!.bufferedReader().lines()
