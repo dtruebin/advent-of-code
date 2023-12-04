1eeb24e991454c06e9644b6dda7e96ac37f7da11
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2023-12-04 02:15:12 +0100

    Put parametrized test arguments closer to the tests

--- src/test/kotlin/icu/trub/AocUtilKtTest.kt
+++ src/test/kotlin/icu/trub/AocUtilKtTest.kt
@@ -2,56 +2,43 @@ package icu.trub
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.Arguments.arguments
-import org.junit.jupiter.params.provider.MethodSource
-import java.util.stream.Stream
+import org.junit.jupiter.params.provider.CsvSource
 
 class AocUtilKtTest {
     @ParameterizedTest
-    @MethodSource
+    @CsvSource(
+        "12, 12",
+        "123, 13",
+        "8, 88",
+        "14gxqgqsqqbxfpxnbccjc33eight, 13",
+    )
     fun testGetDoubleDigitFromFirstAndLast(input: String, expectedOutput: Int) {
         assertEquals(expectedOutput, getDoubleDigitFromFirstAndLast(input))
     }
 
     @ParameterizedTest
-    @MethodSource
+    @CsvSource(
+        "one, 1",
+        "onetwo, 12",
+        "one5two, 152",
+    )
     fun testReplaceWordsWithDigits(input: String, expectedOutput: String) {
         assertEquals(expectedOutput, replaceWordsWithDigits(input))
     }
 
     @ParameterizedTest
-    @MethodSource
+    @CsvSource(
+        "two1nine, 29",
+        "eightwothree, 83",
+        "abcone2threexyz, 13",
+        "xtwone3four, 24",
+        "4nineeightseven2, 42",
+        "zoneight234, 14",
+        "7pqrstsixteen, 76",
+        "one, 11",
+    )
     fun testDay1bCombo(input: String, expectedOutput: Int) {
         assertEquals(expectedOutput, getDoubleDigitFromFirstAndLast(replaceWordsWithDigits(input)))
     }
 
-    companion object {
-        @JvmStatic
-        fun testGetDoubleDigitFromFirstAndLast(): Stream<Arguments> = Stream.of(
-            arguments("12", 12),
-            arguments("123", 13),
-            arguments("8", 88),
-            arguments("14gxqgqsqqbxfpxnbccjc33eight", 13),
-        )
-
-        @JvmStatic
-        fun testReplaceWordsWithDigits(): Stream<Arguments> = Stream.of(
-            arguments("one", "1"),
-            arguments("onetwo", "12"),
-            arguments("one5two", "152"),
-        )
-
-        @JvmStatic
-        fun testDay2Combo(): Stream<Arguments> = Stream.of(
-            arguments("two1nine", 29),
-            arguments("eightwothree", 83),
-            arguments("abcone2threexyz", 13),
-            arguments("xtwone3four", 24),
-            arguments("4nineeightseven2", 42),
-            arguments("zoneight234", 14),
-            arguments("7pqrstsixteen", 76),
-            arguments("one", 11),
-        )
-    }
 }
