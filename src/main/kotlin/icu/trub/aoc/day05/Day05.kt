36b96afd84adfff930afb01abb69b17869aa8ede
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2023-12-09 22:49:45 +0100

    Skip Day 5, Part Two
    Got to take a break.

--- src/main/kotlin/icu/trub/aoc/day05/Day05.kt
+++ src/main/kotlin/icu/trub/aoc/day05/Day05.kt
@@ -6,10 +6,15 @@ import icu.trub.aoc.AocUtil
 class Day05(inputFileName: String) : AbstractDay(inputFileName) {
     override fun solve(): List<Int> = listOf(
         solvePartOne(),
+        solvePartTwo(),
     )
 
     private val almanac: Almanac
         get() = Almanac.parse(AocUtil.readTxtResource(inputFileName).toList())
 
     private fun solvePartOne(): Int = almanac.locations.min().toInt() // phew, it fits
+
+    private fun solvePartTwo(): Int {
+        TODO("Skipped for now")
+    }
 }
